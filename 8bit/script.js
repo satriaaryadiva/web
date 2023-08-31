e54b975188cde96d0b1fe9f34de2a0044e9b345d
satriaaryadiva <121672225+satriaaryadiva@users.noreply.github.com>
2023-08-31 20:31:35 +0700

    Update script.js

--- 8bit/script.js
+++ 8bit/script.js
@@ -25,9 +25,82 @@ contimage.addEventListener('click',function(e){
         setTimeout(
             function(){
                     jumbo.classList.remove('.fade');
+                    
             }
+           
         );
+        setInterval(contimage,1000);
     }
 
-
 });
+
+
+window.addEventListener('scroll', function() {
+    const rect = cards.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    if (rect.top < windowHeight && rect.bottom > 0) {
+        cards.style.backgroundPosition = `-${rect.top}px`;
+    } else {
+        cards.style.backgroundPosition = 'center';
+    }
+});
+
+
+
+
+// scrol
+
+const cards = document.querySelector('.cards');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+
+const scrollStep = 200;
+let currentScroll = 0;
+
+function scrollLeft() {
+    currentScroll -= scrollStep;
+    if (currentScroll < 0) {
+        currentScroll = 0;
+    }
+    cards.scrollTo({
+        left: currentScroll,
+        behavior: 'smooth'
+    });
+}
+
+function scrollRight() {
+    currentScroll += scrollStep;
+    cards.scrollTo({
+        left: currentScroll,
+        behavior: 'smooth'
+    });
+}
+
+leftArrow.addEventListener('mouseover', scrollLeft);
+rightArrow.addEventListener('mouseover', scrollRight);
+
+// scroll efect
+
+
+
+const elements = document.querySelectorAll('*'); // Select all elements within the container
+
+function fadeInAll() {
+    elements.forEach(element => {
+        if (isElementInViewport(element)) {
+            element.classList.add('animate__animated', 'animate__fadeIn');
+        }
+    });
+}
+
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.bottom >= 0
+    );
+}
+
+window.addEventListener('scroll', fadeInAll);
+window.addEventListener('load', fadeInAll); // To trigger on page load
